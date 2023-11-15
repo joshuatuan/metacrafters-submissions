c4d4833edccf3d22d31a127851fc880204d55c5a
joshuatuan <104915069+joshuatuan@users.noreply.github.com>
2023-11-15 20:58:10 +0800

    Update module1_project.sol

--- eth+avax_project_module_1/module1_project.sol
+++ eth+avax_project_module_1/module1_project.sol
@@ -1,16 +1,6 @@
-
 // SPDX-License-Identifier: MIT
 
-/* 
-ETH + AVAX PROOF: Intermediate EVM Course 
-Functions and Errors Project
-
-Project requirements:
-write a smart contract that implements the require(), assert() and revert() statements.
-*/
-
 pragma solidity ^0.8.18;
-
 
 contract HelloWorld {
     string public greet = "Hello World!";
@@ -26,13 +16,20 @@ contract HelloWorld {
         greet = greeting;
     }
 
-    function revertTest(uint256 x) public pure{
-        if (x <= 10) {
-            revert("Revert: Input must be greater than 10");
-        }
+    function isOwner() public view returns (bool _isOwner){
+        if (msg.sender != owner)
+            _isOwner = false;
+            revert("Only the owner can access this function");
     }
-    
-    function assertTest() public pure{
-        assert(1 == 2); // always triggers an assert and revert the transaction.
+
+    function mathOperation(uint256 a, uint256 b) external pure returns (uint256 result) {
+        // Ensure that a + b is always greater than 100
+        assert((a + b) > 100);
+
+        // Perform the calculation
+        result = (a + b);
+
+        // The function will only reach this point if the assert condition is satisfied
+        return result;
     }
 }
